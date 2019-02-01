433683cb201a628665f9276ba0e77cea22732b4c
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-02-01 18:27:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,10 @@
 def my_collect(array)
-    
+    new_array = []
     i = 0
-
     while i < array.length
-      yield(array[i])
+      new_array.push(yield(array[i]))
       i += 1
     end
-    
+    p new_array
 end
 
